f1223bd62cf981050207dfde713f32dfd87fcb7e
HYDRA05 <tanushreesahoo720@gmail.com>
2025-08-11 16:57:30 +0530

    Add task2.py ad output .txt.py for assignment two

---
+++ output.txt.py
@@ -0,0 +1,9 @@
+Enter text to write to the file:Hello, python!'
+data successfully written to output.txt.
+
+Enter additional text to append: Learning file handling in python.
+Data successfully appended.
+
+Final content of output.txt:
+Hello, python!
+Learning file handling in python.
